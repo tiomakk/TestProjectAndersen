4fa1e2ffa4d2455e69b4d0b8b15942d4e2b49a0c
tiomakk <tiomakk@gmail.com>
2018-01-11 21:09:29 +0300

    Забыл добавить коммит после создания проекта для DAL

---
+++ TestProjectAndersen.DAL/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectAndersen.DAL
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> abc = new List<int>()
+            {
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            };
+
+            Console.WriteLine(String.Join(" ",abc.AsQueryable().Where(i => i % 2 == 0).ToList()));
+            Console.ReadKey();
+        }
+       
+    }
+}
