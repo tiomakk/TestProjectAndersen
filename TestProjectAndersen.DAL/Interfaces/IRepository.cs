e926cc39249139f3dc6c107466a863c0013aa833
tiomakk <tiomakk@gmail.com>
2018-01-11 21:04:04 +0300

    Добавил интерфейсы репозитория и UOW

---
+++ TestProjectAndersen.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectAndersen.DAL.Interfaces
+{
+    interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(int id);
+        Task<IEnumerable<T>> FindAsync(Func<T, bool> predicate);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
---
+++ TestProjectAndersen.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProjectAndersen.DAL.Entites;
+
+namespace TestProjectAndersen.DAL.Interfaces
+{
+    interface IUnitOfWork: IDisposable
+    {
+        IRepository<User> Users { get; }
+        IRepository<Program> Orders { get; }
+        IRepository<UserProgram> UserPrograms { get; }
+        IRepository<Metric> Metrics { get; }
+        IRepository<MetricMesurement> MetricMesurements { get; }
+        Task SaveAsync();
+    }
+}
